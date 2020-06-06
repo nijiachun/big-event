b52611f037dcd8e4a710b48234da8559936671a9
nijiachun <1449173318@qq.com>
2020-06-06 14:57:32 +0800

    完成了表单验证

--- assets/js/login.js
+++ assets/js/login.js
@@ -26,5 +26,28 @@ $(function () {
                 }
             }
         });
+    });
+
+    // 注册的表单验证
+    // 使用layui的内置模块，必须先加载
+    var form = layui.form;
+    form.verify({
+        // 自定义验证规则
+        // 键（验证规则）： 值（验证方法）
+        // len: [/^[\S]{6,12}$/, '密码长度不对'] // 使用数值来验证
+        len: function (val) {
+            // val 表示使用该验证规则的输入框的值
+            if (val.trim().length < 6 || val.trim().length > 12) {
+                return "密码长度必须是6~12位,且不能有空格";
+            }
+        },
+        same: function (val) {
+            // val 表示重复密码的值
+            var password = $('.pass').val(); // 第一个密码框的值
+            if (password !== val) {
+                return "两次密码不一致";
+            }
+        }
+
     })
 });
